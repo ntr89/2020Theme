6a1bc5c73a6e2a9ee503586c295ef68ebfde806a
Nick P <npotapov2@gmail.com>
2020-07-15 23:57:38 -0400

    switching from keydown to keyup  on search function to give funciton time to run

--- source/js/search.js
+++ source/js/search.js
@@ -9,6 +9,8 @@ class Search {
     this.events();
     this.overlayActive = false;
     this.typingTimer;
+    this.spinnerVisible = false;
+    this.lastValue;
   }
 
   // all the search events
@@ -18,24 +20,41 @@ class Search {
     this.searchOverlay.on("click", this.closeOverlay.bind(this));
     this.searchInput.on("click", this.focusSearch.bind(this));
     jQuery(document).on("keydown", this.keyPressDispatcher.bind(this));
-    this.searchInput.on("keydown", this.typingLogic.bind(this));
+    this.searchInput.on("keyup", this.typingLogic.bind(this));
   }
   // methods
   typingLogic() {
-    clearTimeout(this.typingTimer);
-    this.searchResults.html('<div class="spinner"></div>');
-    this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+    if (this.searchInput.val() != this.lastValue) {
+      clearTimeout(this.typingTimer);
+
+      if (this.searchInput.val()) {
+        if (!this.spinnerVisible) {
+          this.searchResults.html('<div class="spinner"></div>');
+          this.spinnerVisible = true;
+        }
+        this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+      } else {
+        this.searchResults.html("");
+        this.spinnerVisible = false;
+      }
+    }
+    this.lastValue = this.searchInput.val();
   }
 
   getResults() {
     this.searchResults.html("testing");
+    this.spinnerVisible = false;
     console.log("wooo");
   }
 
   keyPressDispatcher(e) {
     // console.log(e.keyCode);
 
-    if (e.keyCode == 83 && !this.overlayActive) {
+    if (
+      e.keyCode == 83 &&
+      !this.overlayActive &&
+      jQuery("input, textarea").is(":focus")
+    ) {
       this.openOverlay();
     }
     if (e.keyCode == 27 && this.overlayActive) {
