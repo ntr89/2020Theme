1635025cf590e6cf2bd415bb2ffa4d10f4262355
Nick P <npotapov2@gmail.com>
2020-07-19 22:15:08 -0400

    made searching multiple post types asyncronous

--- source/js/search.js
+++ source/js/search.js
@@ -43,33 +43,31 @@ class Search {
   }
 
   getResults() {
-    jQuery.getJSON(
-      localSite.root_url +
-        "/wp-json/wp/v2/project?search=" +
-        this.searchInput.val(),
-      (data) => {
+    jQuery
+      .when(
+        jQuery.getJSON(
+          localSite.root_url +
+            "/wp-json/wp/v2/project?search=" +
+            this.searchInput.val()
+        ),
         jQuery.getJSON(
           localSite.root_url +
             "/wp-json/wp/v2/pages?search=" +
-            this.searchInput.val(),
-          (pages) => {
-            var combineResults = data.concat(pages);
-            this.searchResults.html(`
-        <h2>General Info<h2>
-        ${combineResults.length ? "<ul>" : "<p>No results found</p>"}
-        ${combineResults
-          .map(
-            (item) =>
-              `<li><a href="${item.link}">${item.title.rendered}</a></li>`
-          )
-          .join("")}
-          ${combineResults.length ? "</ul>" : ""}
-        `);
-            this.spinnerVisible = false;
-          }
-        );
-      }
-    );
+            this.searchInput.val()
+        )
+      )
+      .then((data, pages) => {
+        var combineResults = data[0].concat(pages[0]);
+        this.searchResults.html(`
+  <h2>General Info<h2>
+  ${combineResults.length ? "<ul>" : "<p>No results found</p>"}
+  ${combineResults
+    .map((item) => `<li><a href="${item.link}">${item.title.rendered}</a></li>`)
+    .join("")}
+    ${combineResults.length ? "</ul>" : ""}
+  `);
+        this.spinnerVisible = false;
+      });
   }
 
   keyPressDispatcher(e) {
