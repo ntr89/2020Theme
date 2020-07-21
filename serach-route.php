f676655b92acfd8e85be970e7c35d01575103df4
Nick P <npotapov2@gmail.com>
2020-07-20 22:12:29 -0400

    adding custom restAPI route disables portrait animation, need to figure out why. includes dont work with minification

---
+++ serach-route.php
@@ -0,0 +1,1 @@
+add_action('rest_api-init', 'siteSearch'); function siteSearch() { register_rest_route('ntrSite/v1', 'search', array( 'methods' => WP_REST_SERVER::READABLE, 'callback' => 'ntrSearchResults' )); } function ntrSearchResults() { return 'Route test'; };
