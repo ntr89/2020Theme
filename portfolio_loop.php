3ab714ce06a818124aadba885417086053228d28
Nick P <npotapov2@gmail.com>
2020-07-26 01:52:50 -0400

    animation of portfolio description, rotation weird

--- portfolio_loop.php
+++ portfolio_loop.php
@@ -1,5 +1,3 @@
-<section class="vertical-scrolling"><div class="container"><div class="project_summary" style="background-color:<?php  the_field( 'project_color' ); ?>"><div class="main_image"> <?php  the_post_thumbnail( 'full' ); ?> </div><div class="description"><h3 class=""><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3><p><?php if (has_excerpt()) {
-                    echo get_the_excerpt();
-                  } else {
-                      
-                    } ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p></div></div></div></section>
+<section class="vertical-scrolling"><div class="container"><a href="<?php the_permalink(); ?>" class="project_summary" style="background-color:<?php  the_field( 'project_color' ); ?>"><div class="main_image"> <?php  the_post_thumbnail( 'full' ); ?> </div><div class="description supershadow"><h3 class=""><?php the_title(); ?></h3><p><?php if (has_excerpt()) {
+          echo get_the_excerpt();
+        } else {} ?> </p></div></a></div></section>
