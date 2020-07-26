54def645c840f46a707bd84313ec5959f22ff61c
Nick P <npotapov2@gmail.com>
2020-07-26 00:17:09 -0400

    smoothing transitions by switching to translate from left/top

--- portfolio_loop.php
+++ portfolio_loop.php
@@ -1,4 +1,4 @@
-<section class="vertical-scrolling"><div class="container"><div class="project_summary"><div class="main_image"> <?php  the_post_thumbnail( 'full' ); ?> </div><div class="description"> <?php  the_field( 'project_color' ); ?> <h3 class=""><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3><p><?php if (has_excerpt()) {
+<section class="vertical-scrolling"><div class="container"><div class="project_summary" style="background-color:<?php  the_field( 'project_color' ); ?>"><div class="main_image"> <?php  the_post_thumbnail( 'full' ); ?> </div><div class="description"><h3 class=""><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3><p><?php if (has_excerpt()) {
                     echo get_the_excerpt();
                   } else {
                       
--- source/portfolio_loop.php
+++ source/portfolio_loop.php
@@ -2,12 +2,11 @@
          
           <section class="vertical-scrolling">
           <div class="container">
-              <div class="project_summary">
+              <div class="project_summary" style="background-color:<?php  the_field( 'project_color' ); ?>">
                 <div class="main_image">
                 <?php  the_post_thumbnail( 'full' ); ?>
 </div>
                 <div class="description">
-                <?php  the_field( 'project_color' ); ?>
                 <h3 class=""><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
                 <p><?php if (has_excerpt()) {
                     echo get_the_excerpt();
