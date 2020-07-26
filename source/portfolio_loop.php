3a5e4bf7cd4b96f3b900db6f8722d143c8f2aab3
Nick P <npotapov2@gmail.com>
2020-07-25 23:46:13 -0400

    project section styling

--- source/portfolio_loop.php
+++ source/portfolio_loop.php
@@ -2,14 +2,18 @@
          
           <section class="vertical-scrolling">
             
-              <div class="event-summary__content">
+              <div class="project_summary">
+                <div class="main_image">
                 <?php  the_post_thumbnail( 'full' ); ?>
+</div>
+                <div class="description">
                 <h3 class=""><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
                 <p><?php if (has_excerpt()) {
                     echo get_the_excerpt();
                   } else {
                       
                     } ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+                    </div>
               </div>
            
             </section>
