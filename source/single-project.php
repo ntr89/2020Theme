6bd368c9f9bec43afc9ef1880ccb70997f947aae
Nick P <npotapov2@gmail.com>
2020-07-27 23:03:53 -0400

    single project template start

---
+++ source/single-project.php
@@ -0,0 +1,25 @@
+<?php
+
+get_header(); ?>
+<div id="fullpage">
+    
+<section class="vertical-scrolling">
+        
+            <div class="container container--narrow page-section">
+              
+            <?php
+            while(have_posts()) {
+                the_post(); 
+                $content = get_the_content();
+                echo $content;
+            } ?>
+            </div>
+        
+</section>
+
+ 
+</div>
+
+<?php get_footer();
+
+?>
