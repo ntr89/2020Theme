87a688472b446db518f13ae3569ce0535027c62f
Nick P <npotapov2@gmail.com>
2020-07-28 01:38:26 -0400

    header work on protfolio page, need to move first image from WP block to php in header section to use ACF field

--- source/single-project.php
+++ source/single-project.php
@@ -5,8 +5,12 @@ get_header(); ?>
 <div id="page_header" style="background-color:<?php  the_field( 'project_color' ); ?>">
     <div class="container">
     <h1><?php echo the_title(); ?></h1>
+    <?php if( get_field('project_url') ): ?>
+	<button href="<?php the_field('project_url'); ?>" class="btn-6 supershadow">Visit Website <span></span></button>
+<?php endif; ?>
              </div>
              </div>
+             <div class="clearfix"></div>
             <div class="container container--narrow page-section">
                 
             <?php
